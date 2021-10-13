dcaeff99344f27bb10607457dd5443fe63778c83
danielrbimo <hantorobimo@gmail.com>
2021-10-13 18:19:22 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -24,10 +24,20 @@ let svg1 = d3.select('#d3-container')
 .style('border', 'solid')
 .attr('viewBox', [-70, -50, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+//SVG that will hold the visualization
+let svg2 = d3.select('#d3-container')
+.append('svg')
+.attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
+.attr('width', '60%') // this is now required by Chrome to ensure the SVG shows up at all
+.style('background-color', 'white')
+.style('border', 'solid')
+.attr('viewBox', [-70, -10, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+
+
 //updates the charts
 function update(Variables) {
   d3.csv("data/data.csv").then ( function(data) {
-    d3.selectAll("g > *").remove();
+    svg1.selectAll("g > *").remove();
 
     //sorting the data
     if (Variables == 'ascending') {
@@ -80,7 +90,7 @@ function update(Variables) {
         const tooltip_pop = d3.select('#Y');
 
         // Add mouse event to show the tooltip when hovering bars
-        d3.selectAll('.bar')
+        svg1.selectAll('.bar')
         .on('mouseover', function () {
           tooltip.style('visibility', 'visible');
         })
@@ -114,7 +124,102 @@ function update(Variables) {
         .text("Y");
       });
 
-    }
+    };
 
     //initiating the graph
     update('alphabet')
+
+    d3.csv("data/covid.csv").then ( function(data) {
+      const subgroups = data.columns.slice(1,3)
+
+      const groups = data.map(d => d.Country)
+
+      // Adding the X axis
+      const x = d3.scaleBand()
+      .range([ 0, width])
+      .domain(data.map(d => d.Country))
+      .padding(0.5);
+      svg2.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+      // Adding the Y axis
+      const y = d3.scaleLinear()
+      .domain([0, 45000])
+      .range([ height, 0]);
+      svg2.append("g")
+      .call(d3.axisLeft(y));
+
+      const xSubgroup = d3.scaleBand()
+      .domain(subgroups)
+      .range([0, x.bandwidth()])
+      .padding([0.05])
+
+      const color = d3.scaleOrdinal()
+      .domain(subgroups)
+      .range(['#e41a1c','#377eb8'])
+
+      svg2.append("g")
+      .selectAll(".bar2")
+    // Enter in data = loop group per group
+      .data(data)
+      .join("g")
+      .attr("transform", d => `translate(${x(d.Country)}, 0)`)
+      .selectAll("rect")
+      .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+      .join("rect")
+      .attr("class", "bar2")
+      .attr("x", d => xSubgroup(d.key))
+      .attr("y", d => y(d.value))
+      .transition()
+      .duration(750)
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", d => height - y(d.value))
+      .attr("fill", d => color(d.key));
+
+      // create a tooltip
+      const tooltip2 = d3.select('#tooltip2');
+      const tooltip_name2 = d3.select('#Country');
+      const tooltip_pop1 = d3.select('#Cases');
+      const tooltip_pop2 = d3.select('#Deaths');
+
+      // Add mouse event to show the tooltip when hovering bars
+      svg2.selectAll('.bar2')
+      .on('mouseover', function () {
+        tooltip2.style('visibility', 'visible');
+      })
+      .on('mousemove', function (e, d) {
+        tooltip2
+        .style('top', event.pageY - 10 + 'px')
+        .style('left', event.pageX + 10 + 'px');
+        tooltip_name2.text(d3.select(this.parentNode).datum().Country);
+        console.log(d3.select(this.parentNode).datum().Country)
+        tooltip_pop1.text(`Cases: ${d3.select(this.parentNode).datum().Cases}`);
+        console.log(d3.select(this.parentNode).datum().Cases)
+        tooltip_pop2.text(`Deaths: ${d3.select(this.parentNode).datum().Deaths}`);
+      })
+      .on('mouseout', function () {
+        tooltip2.style('visibility', 'hidden');
+      });
+
+      //adding the x label
+      svg2.append("text")
+      .attr("class", "x label")
+      .attr("text-anchor", "end")
+      .attr("x", width - 50)
+      .attr("y", height + 70)
+      .text("40 Countries with the highest covid cases");
+
+      //adding the y label
+      svg2.append("text")
+      .attr("class", "y label")
+      .attr("text-anchor", "end")
+      .attr("y", 10)
+      .attr("x", -100)
+      .attr("dy", "-2.8em")
+      .attr("transform", "rotate(-90)")
+      .text("Covid cases and deaths");
+    })
